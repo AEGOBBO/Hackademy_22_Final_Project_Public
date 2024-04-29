852a6d52dafe3f4b6f13321d00fac22d5aeb2dd5
luca nolano <nolano.luc@gmail.com>
2024-04-29 21:23:56 +0200

    user story-2 annunci homepage

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Advertisement;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
     public function homepage() {
-        return view('welcome');
+        $advertisements=Advertisement::take(5)->get()->sortByDesc('created_at');
+        return view('welcome',compact('advertisements'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,5 +7,22 @@
                 </div>
             </div>
         </div>
+        <div class="container">
+            <div class="row">
+                @foreach ($advertisements as $advertisement)
+                <div class="col-12 col-md-4">
+                    <img src="https://picsum.photos/346" alt="foto" class="card-img-top p-3 rounded">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$advertisement->title}}</h5>
+                        <p class="card-text">{{$advertisement->price}}</p>
+                        <p class="card-text">{{$advertisement->description}}</p>
+                        <a href="" class="btn btn-danger">Dettaglio</a>
+                        <a href="" class="btn btn-success">Categoria: {{$advertisement->category->name}}</a>
+                        <p class="card-footer">pubblicato il: {{$advertisement->created_at->format('d/m/Y')}}</p>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
     </header>
 </x-layout>
