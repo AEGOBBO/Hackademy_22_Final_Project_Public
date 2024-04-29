63552eded3644ad544b9006a8171d47eb3c263a3
AEGOBBO <andregobbo_6@hotmail.com>
2024-04-29 19:15:37 +0200

    AppServiceProvider Issue solved

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
@@ -13,6 +14,6 @@ class Category extends Model
 
     public function advertisements(): HasMany
     {
-        return $this->hasMany(Advertisements::class);
+        return $this->hasMany(Advertisement::class);
     }
 }
