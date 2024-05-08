6dd21e386b9edf834099305ff14a5b971026fc5f
Giovanni <giovanniaddante94@gmail.com>
2024-05-08 20:26:17 +0200

    importata classe

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Advertisement;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
