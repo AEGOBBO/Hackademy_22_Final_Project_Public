442117a1b67cf584b42340502bb3257b338d3e9a
luca nolano <nolano.luc@gmail.com>
2024-05-14 19:42:43 +0200

    user 8 conclusa

--- app/Livewire/AdvertisementCreateForm.php
+++ app/Livewire/AdvertisementCreateForm.php
@@ -10,6 +10,7 @@ use App\Models\Advertisement;
 use Livewire\WithFileUploads;
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
+use App\Jobs\RemoveFaces;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 
@@ -72,9 +73,11 @@ class AdvertisementCreateForm extends Component
                     $newFileName = "advertisements/{$this->advertisement->id}";
                     $newImage = $this->advertisement->images()->create(['path'=>$image->store($newFileName, 'public')]);
 
-                    dispatch(new ResizeImage($newImage->path, 400, 300));
-                    dispatch(new GoogleVisionSafeSearch($newImage->id));
-                    dispatch(new GoogleVisionLabelImage($newImage->id));
+                    RemoveFaces::withChain([
+                        new ResizeImage($newImage->path, 400, 300),
+                        new GoogleVisionSafeSearch($newImage->id),
+                        new GoogleVisionLabelImage($newImage->id)
+                    ])->dispatch($newImage->id);
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
